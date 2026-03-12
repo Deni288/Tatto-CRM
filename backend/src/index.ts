825d60d8eb2345a92047e56e49afe0f956845683
Deni288 <kasic21@gmail.com>
2026-03-12 20:09:53 +0100

    fix:Ažuriranje CORS postavka

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,7 +13,13 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(helmet());
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173',
+    'https://tatto-crm-mu.vercel.app'
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 // Request logger
